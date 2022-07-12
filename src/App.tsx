693aaa3651d570cf93b3d045abb2d19e22857908
Gregory <gkk978@gmail.com>
2022-07-12 17:06:06 +0300

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react'
 import './App.css'
+import loadable from '@loadable/component'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Routes, Route } from 'react-router-dom'
 import RequireAuth from './hoc/RequireAuth'
 import { Provider } from 'react-redux'
 import store from './stor/configurateStore'
 import Recipes from './pages/Recipes'
-import Recipe from './pages/Recipe'
-import CreateRecipe from './pages/CreateRecipe'
-import EditRecipe from './pages/EditRecipe'
+// import Recipe from './pages/Recipe'
+// import CreateRecipe from './pages/CreateRecipe'
+// import EditRecipe from './pages/EditRecipe'
 
 import Sections from './pages/Sections'
-import CreateSection from './pages/CreateSection'
+// import CreateSection from './pages/CreateSection'
+
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Layout from './components/Layout/Layout'
-import Section from './pages/Section'
+// import Section from './pages/Section'
 
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
+// import LoginPage from './pages/LoginPage'
+// import RegisterPage from './pages/RegisterPage'
 
 import { StateProvider } from './contexts/stateProvider'
+
+const CreateSection = loadable(() => import('./pages/CreateSection'))
+const LoginPage = loadable(() => import('./pages/LoginPage'))
+const RegisterPage = loadable(() => import('./pages/RegisterPage'))
+const CreateRecipe = loadable(() => import('./pages/CreateRecipe'))
+const EditRecipe = loadable(() => import('./pages/EditRecipe'))
+const Recipe = loadable(() => import('./pages/Recipe'))
+const Section = loadable(() => import('./pages/Section'))
 
 function App() {
   return (
@@ -34,12 +44,15 @@ function App() {
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
               <Route path="recipes" element={<Recipes />} />
-              <Route path="recipes/:name" element={<Recipe />} />
+              <Route
+                path="recipes/:name"
+                element={<Recipe fallback={<div>Loading...</div>} />}
+              />
               <Route
                 path="recipes/:name/edit"
                 element={
                   <RequireAuth>
-                    <EditRecipe />
+                    <EditRecipe fallback={<div>Loading...</div>} />
                   </RequireAuth>
                 }
               />
@@ -47,13 +60,16 @@ function App() {
                 path="recipes/new"
                 element={
                   <RequireAuth>
-                    <CreateRecipe />
+                    <CreateRecipe fallback={<div>Loading...</div>} />
                   </RequireAuth>
                 }
               />
 
               <Route path="sections" element={<Sections />} />
-              <Route path="sections/:name" element={<Section />} />
+              <Route
+                path="sections/:name"
+                element={<Section fallback={<div>Loading...</div>} />}
+              />
               <Route
                 path="sections/new"
                 element={
